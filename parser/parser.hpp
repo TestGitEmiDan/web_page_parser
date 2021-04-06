215c9e9c95081c2ecf80beba561297cf34842a5c
Danil Sirazeev <sirazev1398@mail.ru>
2021-04-06 17:31:44 +0300

    changed the parsing structure

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -8,7 +8,7 @@ class parser
 {
 public:    
     std::vector<std::pair<std::string, std::string>> get_companies_url_list(const std::string& companies_html);
-   std::vector<std::pair<std::string, std::string>> get_company_info(const std::string company_html);
+    std::vector<std::pair<std::string, std::string>> get_company_info(const std::string company_html);
 
 private:
     static std::pair<std::string, std::string> parse_company_name_and_url(const std::string& company_html);
